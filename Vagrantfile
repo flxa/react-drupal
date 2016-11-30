bd5fd5795f9dec2aa14d0700d35784186c805aaf
Kim Pepper <kim@pepper.id.au>
2016-11-30 11:46:05 +1100

    Rename variable to APP_NAME

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,7 @@
 Vagrant.configure("2") do |config|
   config.vm.define 'lamp', primary: true do |lamp|
     lamp.vm.box      = 'pnx/lamp70'
-    lamp.vm.hostname = '{{ app_name }}.dev'
+    lamp.vm.hostname = 'APP_NAME.dev'
 
     # This script is a last chance for Developers to add more
     # configuration to the Vagrant host.
--- app/sites/default/settings.dashboard.php
+++ app/sites/default/settings.dashboard.php
@@ -2,8 +2,8 @@
 
 $config['dashboard_connector.settings']['enabled'] = skpr_config('dashboard.enabled') ?: TRUE;
 $config['dashboard_connector.settings']['base_uri'] = 'https://status.previousnext.com.au';
-$config['dashboard_connector.settings']['client_id'] = '{{ app_name }}';
-$config['dashboard_connector.settings']['site_id'] = '{{ app_name }}' . (getenv('SKIPPER_ENV') ?: 'local');
+$config['dashboard_connector.settings']['client_id'] = 'APP_NAME';
+$config['dashboard_connector.settings']['site_id'] = 'APP_NAME' . (getenv('SKIPPER_ENV') ?: 'local');
 $config['dashboard_connector.settings']['env'] = getenv('SKIPPER_ENV') ?: 'local';
 $config['dashboard_connector.settings']['username'] = skpr_config('dashboard.username') ?: 'connector';
 $config['dashboard_connector.settings']['password'] = skpr_config('dashboard.password') ?: 'TGyUryDscYDhsVcc7NRp6EXK';
