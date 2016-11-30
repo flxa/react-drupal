69c866d534c2f286ea20c0b8f3642dcf2c630862
Kim Pepper <kim@pepper.id.au>
2016-11-30 14:49:04 +1100

    Adds npm shrinkwrap

--- scripts/rename-project.sh
+++ scripts/rename-project.sh
@@ -3,13 +3,10 @@
 # Initialise a new project, renaming placeholders to the project name.
 # Default to the current directory name.
 APP_NAME=$1
-TEMPLATE_FILES="app/sites/default/* .skpr.yml .pnxci.yml gulpfile.js package.json app/themes/APP_NAME_theme/*"
+TEMPLATE_FILES="app/sites/default/* .skpr.yml .pnxci.yml gulpfile.js package.json app/themes/APP_NAME_theme/* npm-shrinkwrap.json"
 
 echo "Using APP_NAME:" $APP_NAME
 
-for var in $TEMPLATE_FILES ; do
-  echo Updating $var
-  sed -i '' -e "s/APP_NAME/$APP_NAME/g" $var
-done
+find $TEMPLATE_FILES -type f -exec sed -i '' -e "s/APP_NAME/$APP_NAME/g {} \;
 
 mv app/themes/APP_NAME_theme app/themes/${APP_NAME}_theme
