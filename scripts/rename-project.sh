860c6a2452512d5d0b9cb7ba9760ac48096192f7
Kim Pepper <kim@pepper.id.au>
2016-11-30 14:07:09 +1100

    Add front-end tools

--- scripts/rename-project.sh
+++ scripts/rename-project.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
 # Initialise a new project, renaming placeholders to the project name.
-
-TPLT_FILES="app/sites/default/* .skpr.yml .pnxci.yml"
-
 # Default to the current directory name.
 APP_NAME=$1
+TEMPLATE_FILES="app/sites/default/* .skpr.yml .pnxci.yml gulpfile.js package.json"
 
 echo "Using APP_NAME:" $APP_NAME
 
-for var in $TPLT_FILES ; do
+for var in $TEMPLATE_FILES ; do
   echo Updating $var
   sed -i '' -e "s/APP_NAME/$APP_NAME/g" $var
 done
