d4964b596419080c871d3fa9965c072dc599f37b
Kim Pepper <kim@pepper.id.au>
2016-11-30 14:52:40 +1100

    Switch from forloop to find

--- scripts/rename-project.sh
+++ scripts/rename-project.sh
@@ -3,10 +3,10 @@
 # Initialise a new project, renaming placeholders to the project name.
 # Default to the current directory name.
 APP_NAME=$1
-TEMPLATE_FILES="app/sites/default/* .skpr.yml .pnxci.yml gulpfile.js package.json app/themes/APP_NAME_theme/* npm-shrinkwrap.json"
+TEMPLATE_FILES="app/sites/default/* .skpr.yml .pnxci.yml gulpfile.js package.json app/themes/APP_NAME_theme/*.yml npm-shrinkwrap.json"
 
 echo "Using APP_NAME:" $APP_NAME
 
-find $TEMPLATE_FILES -type f -exec sed -i '' -e "s/APP_NAME/$APP_NAME/g {} \;
+find $TEMPLATE_FILES -type f -exec sed -i '' -e "s/APP_NAME/$APP_NAME/g" {} \;
 
 mv app/themes/APP_NAME_theme app/themes/${APP_NAME}_theme
