f96843d99a19b99c3fa7993e59895d54ad1f2d3e
Jibran Ijaz <jibran@users.noreply.github.com>
2017-05-30 10:08:54 +1000

    Add docs around switch php. (#38)
    Signed-off-by: Jibran Ijaz <jibran@1198144.no-reply.drupal.org>

--- provision.sh
+++ provision.sh
@@ -3,3 +3,7 @@
 # Ensure we have directories.
 sudo mkdir -p /vagrant/app/sites/default/files/tmp /private
 sudo chown -R vagrant:vagrant /vagrant/app/sites/default/files /private
+# The default version is 7.1.
+# switch_php 5.6
+# switch_php 7.0
+# switch_php 7.1
