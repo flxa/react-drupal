b555992ee4bbcee207f4431ffb18ff12c268a923
Jibran Ijaz <jibran@1198144.no-reply.drupal.org>
2017-07-11 09:42:40 +1000

    Change the folder path.
    Signed-off-by: Jibran Ijaz <jibran@1198144.no-reply.drupal.org>

--- scripts/rename-project.sh
+++ scripts/rename-project.sh
@@ -7,12 +7,12 @@ APP_NAME=$1
 TEMPLATE_FILES=".pnxci.yml \
 .skpr.yml \
 .gitignore \
-app/profiles/APP_NAME_profile/* \
-app/profiles/APP_NAME_profile/config/install/* \
+app/profiles/custom/APP_NAME_profile/* \
+app/profiles/custom/APP_NAME_profile/config/install/* \
 app/sites/default/* \
-app/themes/APP_NAME_theme/*.yml \
-app/themes/APP_NAME_theme/APP_NAME_theme.theme \
-app/themes/APP_NAME_theme/src/init/_asset.scss \
+app/themes/custom/APP_NAME_theme/*.yml \
+app/themes/custom/APP_NAME_theme/APP_NAME_theme.theme \
+app/themes/custom/APP_NAME_theme/src/init/_asset.scss \
 docs/* \
 gulpfile.yml \
 Makefile \
@@ -25,14 +25,15 @@ echo "Running find and replace using APP_NAME:" $APP_NAME
 find $TEMPLATE_FILES -type f -exec sed -i '' -e "s/APP_NAME/$APP_NAME/g" {} \;
 
 # Rename profile files
-mv app/profiles/APP_NAME_profile/APP_NAME_profile.install app/profiles/APP_NAME_profile/${APP_NAME}_profile.install
-mv app/profiles/APP_NAME_profile/APP_NAME_profile.info.yml app/profiles/APP_NAME_profile/${APP_NAME}_profile.info.yml
-mv app/profiles/APP_NAME_profile/APP_NAME_profile.post_update.php app/profiles/APP_NAME_profile/${APP_NAME}_profile.post_update.php
-mv app/profiles/APP_NAME_profile app/profiles/${APP_NAME}_profile
+mv app/profiles/custom/APP_NAME_profile/APP_NAME_profile.install app/profiles/custom/APP_NAME_profile/${APP_NAME}_profile.install
+mv app/profiles/custom/APP_NAME_profile/APP_NAME_profile.info.yml app/profiles/custom/APP_NAME_profile/${APP_NAME}_profile.info.yml
+mv app/profiles/custom/APP_NAME_profile/APP_NAME_profile.post_update.php app/profiles/custom/APP_NAME_profile/${APP_NAME}_profile.post_update.php
+mv app/profiles/custom/APP_NAME_profile app/profiles/custom/${APP_NAME}_profile
 
 # Rename theme files
-mv app/themes/APP_NAME_theme/APP_NAME_theme.breakpoints.yml app/themes/APP_NAME_theme/${APP_NAME}_theme.breakpoints.yml
-mv app/themes/APP_NAME_theme/APP_NAME_theme.info.yml app/themes/APP_NAME_theme/${APP_NAME}_theme.info.yml
-mv app/themes/APP_NAME_theme/APP_NAME_theme.libraries.yml app/themes/APP_NAME_theme/${APP_NAME}_theme.libraries.yml
-mv app/themes/APP_NAME_theme/APP_NAME_theme.layouts.yml app/themes/APP_NAME_theme/${APP_NAME}_theme.layouts.yml
-mv app/themes/APP_NAME_theme app/themes/${APP_NAME}_theme
+mv app/themes/custom/APP_NAME_theme/APP_NAME_theme.breakpoints.yml app/themes/custom/APP_NAME_theme/${APP_NAME}_theme.breakpoints.yml
+mv app/themes/custom/APP_NAME_theme/APP_NAME_theme.info.yml app/themes/custom/APP_NAME_theme/${APP_NAME}_theme.info.yml
+mv app/themes/custom/APP_NAME_theme/APP_NAME_theme.libraries.yml app/themes/custom/APP_NAME_theme/${APP_NAME}_theme.libraries.yml
+mv app/themes/custom/APP_NAME_theme/APP_NAME_theme.layouts.yml app/themes/custom/APP_NAME_theme/${APP_NAME}_theme.layouts.yml
+mv app/themes/custom/APP_NAME_theme/APP_NAME_theme.theme app/themes/custom/APP_NAME_theme/${APP_NAME}_theme.theme
+mv app/themes/custom/APP_NAME_theme app/themes/custom/${APP_NAME}_theme
